bdb9e1a2696696f094edd7d5355da02d7d42f64b
Ken <cczxntken@163.com>
2017-12-06 17:18:25 +0800

    Aes加密

---
+++ LitEngine/Script/SteamReaderAndWriter/AESReader.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+namespace LitEngine
+{
+    namespace IO
+    {
+        
+        public class AESReader : AesStreamBase
+        {
+            private RijndaelManaged mRijindael = null;
+            private FileStream mStream = null;
+            private BinaryReader mReaderStream = null;
+            public AESReader(string _filename)
+            {
+                if (!File.Exists(_filename)) throw new System.NullReferenceException(_filename + "Can not found.");
+                mStream = File.OpenRead(_filename);
+                Init(mStream);
+            }
+
+            protected void Init(Stream _stream)
+            {
+                mRijindael = GetRijndael();
+                ICryptoTransform cTransform = mRijindael.CreateDecryptor();
+                CryptoStream cst = new CryptoStream(_stream, cTransform, CryptoStreamMode.Read);
+                mReaderStream = new BinaryReader(cst);
+            }
+            override protected void DisposeStream()
+            {
+                mReaderStream.Close();
+                mStream.Close();
+                mStream.Dispose();
+                mRijindael.Clear();
+            }
+        }
+    }  
+}
---
+++ LitEngine/Script/SteamReaderAndWriter/AesStreamBase.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+namespace LitEngine
+{
+    namespace IO
+    {
+        public abstract class AesStreamBase : System.IDisposable
+        {
+            protected const string AESKey = "ae125efkk4454eeff444ferfkny6oxi8";
+
+            ~AesStreamBase()
+            {
+                Dispose(false);
+            }
+
+            protected bool mDisposed = false;
+            public void Dispose()
+            {
+                Dispose(true);
+                System.GC.SuppressFinalize(this);
+            }
+
+            protected void Dispose(bool _disposing)
+            {
+                if (mDisposed)
+                    return;
+                mDisposed = true;
+                DisposeStream();
+            }
+
+            virtual protected RijndaelManaged GetRijndael()
+            {
+                RijndaelManaged ret = new RijndaelManaged();
+                byte[] keyArray = Encoding.UTF8.GetBytes(AESKey);
+                ret.Key = keyArray;
+                ret.Mode = CipherMode.ECB;
+                ret.Padding = PaddingMode.PKCS7;
+                return ret;
+            }
+
+            abstract protected void DisposeStream();
+
+        }
+    }
+   
+}
